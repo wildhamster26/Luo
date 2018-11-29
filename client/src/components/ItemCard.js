d50520e5fbb6f23d03966cd5cce2eccec8d89da0
carloscadel <c.cazallas.delfa@gmail.com>
2018-11-29 13:44:21 +0100

    card component and styles

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -6,17 +6,20 @@ const ItemCard = ({name, pictures, pricePerPeriod, period, description}) => {
   return (
     <div className="itemCard">
       <div className="itemCard-name">
-        <h2>{name}</h2>
+        <h5>{name}</h5>
       </div>
       <div className="itemCard-img-wrapper">
         <img src={pictures} />
       </div>
       <div className="itemCard-sub-img">
-        <h5>{pricePerPeriod}€ per {period}</h5>
-        <h5>Availability</h5>
+        <h6>{pricePerPeriod}€ per {period}</h6>
+        <h6><a href="#">Availability</a></h6>
       </div>
       <div>
         <p>{description}</p>
+      </div>
+      <div className="itemCard-btn-div">
+        <button>Request</button>
       </div>
     </div>
   )
